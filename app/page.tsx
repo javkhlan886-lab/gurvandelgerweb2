849cc154d0e5670714b2fa3e6701941b13fea2ab
Javkhlan <battsetsegerdenebayar@Battsetsegs-MacBook-Air.local>
2026-06-06 17:54:26 +0800

    home page der hayag oruulav

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import home from "@/components/home";
+
+import Footer from "@/components/footer";
 const Home = home;
 
 export default function Page() {
   return (
     <div>
       <Home />
+      <Footer />
     </div>
   );
 }
